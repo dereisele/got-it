59a45737b08e90d1163ab6a419a4c981a4299795
Alexander Eisele <alexander@eiselecloud.de>
2018-04-04 22:16:02 +0200

    Remove old testing code

--- gotit/extractor/us_sciencechannel_com.py
+++ gotit/extractor/us_sciencechannel_com.py
@@ -9,16 +9,3 @@ class Extractor(us_discovery.Extractor):
     def __init__(self):
         self.NETW = "SCI"
         self.LANG = "en"
-
-    def extract(self):
-        self._initBearer()
-
-        """
-        with Pool(1) as p:
-            rst = p.map(self._getSeasons, self._getShows())
-            pprint(rst)
-        """
-        show = next(self._getShows())
-        season = next(self._getSeasons(show["x"]["x_dsc_show_id"]))
-        episode = next(self._getEpisodes(season["x"]["x_dsc_season_id"]))
-        pprint(episode)
