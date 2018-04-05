6268a69e14f172d42df882e771b73cb25eb4a0df
Alexander Eisele <alexander@eiselecloud.de>
2018-04-05 23:52:17 +0200

    Add getX-parameters and require extractEpisodes

--- gotit/extractor_blueprint/basic.py
+++ gotit/extractor_blueprint/basic.py
@@ -1,10 +1,22 @@
 import requests
 import json
+
+
 class Extractor(object):
     def extract(self):
+        raise NotImplementedError
+
+    def extractShows(self):
+        raise NotImplementedError
+
+    def extractEpisodes(self, showref):
         raise NotImplementedError
 
     def loadJson(self, url, **kwargs):
         r = requests.get(url, **kwargs)
         j = json.loads(r.text)
         return j
+
+    def getX(self, object):
+        """Get x attribute from show as dict."""
+        return json.loads(object.x)
