7826ae3db714562c92020b646d635a0ef4d243b1
Alexander Eisele <alexander@eiselecloud.de>
2018-04-05 23:54:07 +0200

    Prepare for episode scrapping

--- gotit/dbmanager.py
+++ gotit/dbmanager.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
 
 base = declarative_base()
+
+from .models import Show, ShowScraperRef, Scraper
 
 def dbConnect():
     return create_engine("sqlite:////home/alexander/gotit.db")
@@ -19,3 +22,27 @@ def get_or_create(session, model, **kwargs):
         session.add(instance)
         session.commit()
         return instance, True
+
+def getShows():
+    engine = dbConnect()
+    createTables(engine)
+    session = sessionmaker(bind=engine)()
+    shows = session.query(Show)
+    session.close()
+    return shows
+
+
+def getShowScraperRef(ignore_filter=False):
+    engine = dbConnect()
+    createTables(engine)
+    session = sessionmaker(bind=engine)()
+
+    # TODO: for early debugging
+    if ignore_filter:
+        show_scraper_refs = session.query(ShowScraperRef)
+    else:
+        scraper_string = input("scraper_string: ")
+        scraper_id = session.query(Scraper).filter_by(string_id=scraper_string).first().id
+        show_scraper_refs = session.query(ShowScraperRef).filter_by(scraper_id=scraper_id)
+    session.close()
+    return show_scraper_refs
