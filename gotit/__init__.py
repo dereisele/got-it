f8fa28ae8225b7dc3ae492ccb0fa432a9f780695
Alexander Eisele <alexander@eiselecloud.de>
2018-05-04 13:47:26 +0200

    [WIP] Add basic testing mode

--- gotit/__init__.py
+++ gotit/__init__.py
@@ -1,7 +1,47 @@
 # coding: utf-8
 __version__ = '0.1'
+import argparse
 
 from .scrape import ScrapeManager
 
 def main():
-    ScrapeManager()
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument("--debug", help="Start in debug mode",
+                        action="store_true")
+    args = parser.parse_known_args()
+
+    if args[0].debug:
+        debugMode(parser=parser)
+    else:
+        normalMode()
+
+
+def normalMode():
+    s = ScrapeManager()
+
+    s.initPlugins()
+    s.initPipelines()
+
+    s.scrapeShows()
+    s.scrapeEpisodes()
+
+def debugMode(parser):
+    s = ScrapeManager()
+
+    print("DEBUGGING")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test-scraper", "-t-sc", help="Scraper to debug")
+    parser.add_argument("--test-full", "-t-f", help="Extended testing mode",
+                        action="store_true")
+    args = parser.parse_known_args()
+    if args[0].test_scraper:
+        s.initPlugins()
+        if args[0].test_scraper in s.extractors:
+            if args[0].test_full:
+                s.initPipelines()
+                s.scrapeShowsOne(args[0].test_scraper)
+            else:
+                s.testExtractor(args[0].test_scraper)
+        else:
+            print("Error: Scraper not found")
--- gotit/scrape.py
+++ gotit/scrape.py
@@ -10,28 +10,32 @@ class ScrapeManager(object):
 
     def __init__(self):
 
-        self.extractors = {}
-
-        self.initPlugins()
-        self.initPipelines()
-
-        self.scrapeShows()
-        self.scrapeEpisodes()
+        self.extractors = []
 
     def initPlugins(self):
         self.plugin_base = PluginBase(package="gotit.extractors")
         self.plugin_source = self.plugin_base.make_plugin_source(
                                 searchpath=["./gotit/extractor"])
 
-        pprint(self.plugin_source.list_plugins())
+        self.extractors = self.plugin_source.list_plugins()
+        pprint(self.extractors)
 
     def initPipelines(self):
         self.showPip = ShowPipeline()
 
-    def scrape(self):
-        for plugin_name in self.plugin_source.list_plugins():
-            plugin = self.plugin_source.load_plugin(plugin_name)
-            plugin.Extractor().extract()
+    def testExtractor(self, scraperName):
+        plugin = self.plugin_source.load_plugin(scraperName)
+        plugin.Extractor().extractTest()
+
+    def scrapeShowsOne(self, plugin_name, insert=False):
+        plugin = self.plugin_source.load_plugin(plugin_name)
+        shows = plugin.Extractor().extractShows()
+        print(plugin_name)
+        for show in shows:
+            if insert:
+                self.showPip.insertShow(plugin_name, show)
+            pprint(show)
+
 
     def scrapeShows(self):
         for plugin_name in self.plugin_source.list_plugins():
