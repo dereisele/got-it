5c093e8080fdd3ec54f6a3055751c94004413452
Alexander Eisele <alexander@eiselecloud.de>
2018-05-13 11:31:59 +0200

    Use single session

--- gotit/pipelines.py
+++ gotit/pipelines.py
@@ -8,38 +8,40 @@ class ShowPipeline(object):
     def __init__(self):
         engine = dbConnect()
         createTables(engine)
-        self.Session = sessionmaker(bind=engine)
+        self.Session = sessionmaker(bind=engine)()
 
     def insertShow(self, scraper_id, show):
-        session = self.Session()
-
-        dbShow, _ = get_or_create(session, Show,
+        dbShow, _ = get_or_create(self.Session, Show,
                                   name=show["name"],
                                   year=show["year"],
                                   lang=show["lang"])
 
         dbScraper, _ = get_or_create(session, Scraper, string_id=scraper_id)
 
-        get_or_create(session, ShowScraperRef,
+        get_or_create(self.Session, ShowScraperRef,
                       scraper_id=dbScraper.id,
                       show_id=dbShow.id,
                       x=dumps(show.get("x")),
                       url=show["url"])
 
     def insertEpisode(self, showref, episode):
-        session = self.Session()
-
-        dbSeason, _ = get_or_create(session, Season,
+        dbSeason, _ = get_or_create(self.Session, Season,
                                     number=episode["season_number"],
                                     show_id=showref.show_id)
 
-        dbEpisode, _ = get_or_create(session, Episode,
+        dbEpisode, _ = get_or_create(self.Session, Episode,
                                      season_id=dbSeason.id,
                                      number=episode["episode_number"],
                                      name=episode["name"])
 
-        get_or_create(session, EpisodeScraperRef,
+        get_or_create(self.Session, EpisodeScraperRef,
                       episode_id=dbEpisode.id,
                       scraper_id=showref.scraper_id,
                       url=episode["url"],
                       x=dumps(episode.get("x")))
+
+    def __del__(self):
+        try:
+            self.Session.close()
+        except AttributeError:
+            pass
